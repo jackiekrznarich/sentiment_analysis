9a59282b8cf9e174b78dbc8493a0a8d5379b89b8
jackiekrznarich <jkrznari@umich.edu>
2023-03-13 17:47:01 -0400

    data to work with, article extraction

--- analysis/extract_articles.js
+++ analysis/extract_articles.js
@@ -1,5 +1,8 @@
 const axios = require("axios");
 const config = require('../config');
+const fs = require('fs');
+
+// Get text from articles
 
 async function getText(link) {
     console.log("Getting article text for " + link);
@@ -18,21 +21,47 @@ async function getText(link) {
     data: encodedParams
     };
 
-    axios.request(options).then(function (response) {
-        console.log(response.data);
+    var text = await axios.request(options).then((response) => {
+        return response.data;
     }).catch(function (error) {
         console.error(error);
     });
+    return {link: link, text: text.article.text};
+}
+
+
+async function cleanText(text) {
+    for (article of text) {
+        article.text = article.text.replace(/\n/g, " ");
+    }
+    return text;
+}
+
+// Save to tmp/text
+async function saveText(text) {
+    console.log("Saving to tmp/articleText.json");
+    console.log(text);
+    //create tmp directory if does not exist
+    fs.mkdirSync("tmp", { recursive: true });
+    filename = "tmp/articleText.json";
+    var json = JSON.stringify(text);
+    fs.writeFile(filename, json, (err) => {
+        // In case of a error throw err.
+        if (err) console.log(err);
+    })
+    console.log("Finished writing to " + filename);
 }
 
 // Starting point
 (async () => {
     console.log("Parsing articles");
     var fs = require('fs');
-    var links = fs.readFileSync('../tmp/urls.txt').toString().split("\n");
+    var links = fs.readFileSync('tmp/urls.txt').toString().split("\n");
     var text = [];
     for (var link of links) {
-        text.push(getText(link));
+        text.push(await getText(link));
     }
-    console.log(text);
+    text.push(await getText(links[0]));
+    text = await cleanText(text);
+    saveText({articles: text});
 })();
