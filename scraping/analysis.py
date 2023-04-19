1248fd55fc934396f883a33080641473f3f52f80
jackiekrznarich <jkrznari@umich.edu>
2023-04-18 21:55:44 -0400

    updates

--- scraping/analysis.py
+++ scraping/analysis.py
@@ -1,23 +1,40 @@
 import json
+import csv
 from textblob import TextBlob
 
-with open('../tmp/articleText.json', 'r') as f:
-    data = json.load(f)
 
-# Extract the "text" field from each article in the "articles" list
-text_list = [article['text'] for article in data['articles']]
+def main():
+    with open('../tmp/articleText.json', 'r') as f:
+        data = json.load(f)
+
+    with open('../frontend/my_file.csv', 'r') as userData:
+        reader = csv.reader(userData)
+        userData = []
+        for row in reader:
+            userData.append(row)
+        userData.pop()
+
+    article_info = {}
+    for article in data['articles']:
+        # Print the list of "text" values
+        text = article['text']
+        blob = TextBlob(text)
+
+        # Perform sentiment analysis
+        polarity = blob.sentiment.polarity
+
+        # Print sentiment polarity score
+        # print("Sentiment polarity score:", polarity)
+        article_info[article['link']] = {'text': article['text'], 'polarity': polarity}
+
+    print(article_info)
+
+    # Iterate through users and identify which are watching stocks that have been updated
+    for user in userData:
+
+        tickers = user[3]
 
 
-for text in text_list:
-    # Print the list of "text" values
-    print(text)
 
-    blob = TextBlob(text)
-
-    # Perform sentiment analysis
-    polarity = blob.sentiment.polarity
-
-    # Print sentiment polarity score
-    print("Sentiment polarity score:", polarity)
-
-
+if __name__ == "__main__":
+    main()
